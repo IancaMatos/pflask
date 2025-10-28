7324328ae4225ec37d42b19fa69b6a15d3396dad
Ianca Matos <iancaddd@gmail.com>
2025-10-28 18:31:53 -0300

    Pflask , bdl sqlite

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+from dao.aluno_dao import AlunoDAO
+from dao.professor_dao import ProfessorDAO
+from dao.turma_dao import TurmaDAO
+from dao.matricula_dao import MatriculaDAO
+from dao.curso_dao import CursoDAO
+
 
 app = Flask(__name__)
 
@@ -8,9 +14,39 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/sobre') # nova rota para a página sobre
-def sobre():
-    return render_template('sobre.html')
+@app.route('/aluno')
+def listar_aluno():
+    dao = AlunoDAO()
+    lista = dao.listar()
+    return render_template('aluno/lista.html', lista=lista)
+
+
+@app.route('/professor')
+def listar_professor():
+    dao = ProfessorDAO()
+    lista = dao.listar()
+    return render_template('professor/lista.html', lista=lista)
+
+
+@app.route('/turma')
+def listar_turma():
+    dao = TurmaDAO()
+    lista = dao.listar()
+    return render_template('turma/lista.html', lista=lista)
+
+
+@app.route('/matricula')
+def listar_matricula():
+    dao = MatriculaDAO()
+    lista = dao.listar()
+    return render_template('matricula/lista.html', lista=lista)
+
+
+@app.route('/curso')
+def listar_curso():
+    dao = CursoDAO()
+    lista = dao.listar()
+    return render_template('curso/lista.html', lista=lista)
 
 
 if __name__ == '__main__':
